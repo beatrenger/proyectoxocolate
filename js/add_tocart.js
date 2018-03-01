bfc536fe3bc6f69fc0b37bf76a0491878b41a4d0
Beatrenger <arenteriaperulles@Outlook.com>
2018-02-28 17:44:11 -0800

    more testing

--- js/add_tocart.js
+++ js/add_tocart.js
@@ -2,10 +2,10 @@ jQuery(function($) {
 
   console.log('loaded add_tocart javascript file');
   // ajax call
-  $.post(my_js_data.ajaxurl, { action: 'add_to_cart',  }, function(output) {
-     console.log(output);
-     console.log('thefuck');
-  });
+  // $.post(my_js_data.ajaxurl, { action: 'add_to_cart',  }, function(output) {
+  //    console.log(output);
+  //    console.log('thefuck');
+  // });
 
 
 });
