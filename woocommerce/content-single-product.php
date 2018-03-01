a3a986f3a64f6cebfce9406a88ddbf1e9ca4f494
Beatrenger <arenteriaperulles@Outlook.com>
2018-02-28 17:38:52 -0800

    more testing

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -15,60 +15,209 @@
  * @package 	WooCommerce/Templates
  * @version     3.0.0
  */
+
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+	exit; // Exit if accessed directly
 }
-/**
- * Hook Woocommerce_before_single_product.
- *
- * @hooked wc_print_notices - 10
- */
-do_action( 'woocommerce_before_single_product' );
-if ( post_password_required() ) {
-	echo get_the_password_form(); // WPCS: XSS ok.
-	return;
-}
+
 ?>
-<div id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
+ este es el conteint-single-product.php
+<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php
-		/**
-		 * Hook: woocommerce_before_single_product_summary.
-		 *
-		 * @hooked woocommerce_show_product_sale_flash - 10
-		 * @hooked woocommerce_show_product_images - 20
-		 */
-		do_action( 'woocommerce_before_single_product_summary' );
-	?>
+<?php
+global $product;
+				  $attachment_ids = $product->get_gallery_image_ids(); //product gallery
+					$titulo = $product->get_title(); // titulo
+					$precio =  $product->get_price(); // precio
 
-	<div class="summary entry-summary">
-		<?php
-			/**
-			 * Hook: Woocommerce_single_product_summary.
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 * @hooked WC_Structured_Data::generate_product_data() - 60
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-		?>
+					if( $product->is_type( 'variable' )){
+						$available_variations = $product->get_variation_attributes(); // atributos
+	}
+					 $s_description = $product->get_short_description(); // short description
+					$l_description = $product->get_description(); // long descpription
+          $upsells = $product->get_upsell_ids();
+
+?>
+<div class="container">
+
+	<div class="row">
+		<div class="titulo col-xs-12">
+			<div class="col-xs-12">
+				<?php echo $titulo ?>
+			</div>
+		</div>
+			<div id="myCarouselProduct" class="col-xs-12 col-md-7 carousel slide" data-ride="carousel">
+
+				<div class="col-xs-12 col-sm-10 col-md-10 main-image">
+					<div class="carousel-inner" role="listbox">
+
+					<?php
+					$first_active= false;
+					foreach( $attachment_ids as $attachment_id ){
+						?>
+							<div class="item <?php  echo ($first_active) ?: 'active'; $first_active = true; ?>">
+									<img   src="<?php echo $image_link = wp_get_attachment_url( $attachment_id );?>"   width="100%" height="auto">
+							</div>
+							<?php
+							 }
+								 ?>
+								 </div>
+					</div>
+
+
+
+								<div class="col-xs-12 col-sm-2 col-md-2">
+
+									<ol class="carousel-indicators">
+
+									<?php
+									$value = 0;
+									foreach( $attachment_ids as $attachment_id ){
+										?>
+
+										 <li data-target="#myCarouselProduct" data-slide-to="<?php echo $value; ?>" class="<?php  echo ($first_active) ?: 'active'; $first_active = true; ?>">
+										<img src="<?php echo $image_link = wp_get_attachment_url( $attachment_id );?>" class="img-thumbnail" alt="Cinque Terre" width="90" height="90">
+										</li>
+
+						<?php
+						$value++;
+					}
+									 ?>
+					 </ol>
+								</div>
+			</div>
+
+			<div id="product_info" class="col-xs-12 col-md-5">
+				<div class="col-xs-12">
+					<div class="precio">
+						<span> $<?php
+								 echo $precio;
+							 ?></span>
+					</div>
+					<?php
+					if($available_variations){
+	 						foreach($available_variations as $variation => $value){
+						?>
+						<div class="form-group">
+						<label for="sel1">Lista de Selecion:</label>
+
+						<select class="form-control" id="<?php echo $variation;?>">
+
+								<?php
+											foreach($value as $v){
+												?>
+												<option value=""> <?php echo $v;?></option>
+												<?php
+											}
+
+							 ?>
+
+						</select>
+					</div>
+						<?php
+						}
+					}
+							 ?>
+
+				</div>
+
+
+	<div class="col-xs-6 cantidad">
+		<span>Cantidad</span>
+	</div>
+	<div class="col-xs-6">
+
+		<div class="input-group-carrito spinner-carrito">
+				<button class="btn btn-brown" type="button"><b>-</b></button>
+				<input type="text" class="form-control" value="0">
+	      <button class="btn btn-brown" type="button"><b>+</b></button>
+
+	    </div>
+	</div>
+	<div class="col-xs-12 sbmit-button">
+		<?php if($product->is_purchasable( )){
+			?>
+				<button type="submit" class="btn btn-px"><b>Agregar al Carrito</b></button>
+			<?php
+		}else{
+			?>
+				<button type="button" class="btn btn-secondary">Desabilitado</button>
+			<?php
+		} ?>
 	</div>
 
-	<?php
-		/**
-		 * Hook: woocommerce_after_single_product_summary.
-		 *
-		 * @hooked woocommerce_output_product_data_tabs - 10
-		 * @hooked woocommerce_upsell_display - 15
-		 * @hooked woocommerce_output_related_products - 20
-		 */
-		do_action( 'woocommerce_after_single_product_summary' );
-	?>
+	<div class="col-xs-12" >
+<div class="short_description">
+		<?php echo $s_description; ?>
 </div>
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+	</div>
+	  </div>
+
+
+	</div>
+
+	<div class="row otrosarticulos">
+		<div class="col-xs-12">
+				<div class="titulo sugerido">
+					Articulos Sugeridos
+				</div>
+
+
+		</div>
+
+
+	</div>
+
+ <div class="row container-cats">
+ 		<?php
+		// assuming the list of product IDs is are stored in an array called IDs;
+$_pf = new WC_Product_Factory();
+
+		foreach ($upsells as $cat) {
+				$_product = $_pf->get_product($cat);
+			 	$image = wp_get_attachment_url( $_product->get_image_id() );
+		 	?>
+			<div class="col-xs-12 col-sm-6 col-md-4" >
+			  <a href="<?php echo $_product->get_permalink(); ?>">
+
+			<div class="item-category"   style="background-image: url(<?php echo $image?>); background-repeat: no-repeat; background-position: center;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;"; >
+
+			<div class="category-title">
+			  <?php echo $_product->get_title(); ?>
+			</div>
+			<div class="hover">
+			    <div class="circle">
+			        <span class="glyphicon glyphicon-plus"></span>
+			    </div>
+			</div>
+			</div>
+
+			</a>
+			 </div>
+		<?php
+		} ?>
+ </div>
+
+ <div class="row">
+ 	 <div class="col-xs-12">
+		 <div class="long_description">
+ 				<?php echo $l_description; ?>
+ 		</div>
+
+ 			</div>
+ 	 </div>
+ </div>
+
+
+
+
+
+
+	</div><!-- #product-<?php the_ID(); ?> -->
+
+</div>
